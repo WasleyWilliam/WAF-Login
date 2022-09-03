6162d672b3cf404fe56ab13ae33048f900191ad1
Wasley William <ww.adm@outlook.com>
2022-09-03 11:23:14 -0300

    Update ModuloConexao.java

--- waflogin/src/br/com/waflogin/dal/ModuloConexao.java
+++ waflogin/src/br/com/waflogin/dal/ModuloConexao.java
@@ -5,10 +5,22 @@
  */
 package br.com.waflogin.dal;
 
+import java.sql.*;
 /**
  *
  * @author wasle
  */
 public class ModuloConexao {
+    //Método Responsável por estabelecer a conexão com o banco
+    
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        // A linha Abaixo Chama o Drive. 
+        String driver = "com.mysql.jdbc.Driver";
+        //Armazenando informações referente ao banco
+        
+    }
+    
+    
     
 }
